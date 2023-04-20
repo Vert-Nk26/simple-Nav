e10f3f1a2390716335fabe8e9be9587d0534582c
Vert Ennin Wilson <87149619+Vert-Nk26@users.noreply.github.com>
2023-04-21 01:33:27 +0200

    10th commit maybe xd

---
+++ index.js
@@ -0,0 +1,26 @@
+const getBurgerMenu = document.getElementById("burger")
+const getNavigation = document.getElementById("navigation")
+const getListItems = Array.from(document.querySelectorAll("li"))
+// const getMenu = document.querySelector(".menu")
+
+getBurgerMenu.addEventListener("click", () => {
+  showNav()
+  navListFadeIn()
+  getBurgerMenu.classList.toggle("close")
+})
+// Show Nav
+const showNav = nav => {
+  getNavigation.classList.toggle("active")
+}
+
+// NavListFAdeIn
+
+const navListFadeIn = () => {
+  getListItems.forEach((navList, indexOf) => {
+    if (navList.style.animation) {
+      navList.style.animation = ""
+    } else {
+      navList.style.animation = `fadeIn .3s forwards ${indexOf / 7 + 0.3}s`
+    }
+  })
+}
